a264cb46ac4f7337ed1617cd5c142989f3f49b1e
randy <randy@192.168.2.32>
2019-04-01 19:20:09 +0200

    Jochem

--- COC/src/me/jerajo/coc/COCEventClass.java
+++ COC/src/me/jerajo/coc/COCEventClass.java
@@ -1,7 +1,13 @@
 package me.jerajo.coc;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class COCEventClass implements Listener {
 
+	@EventHandler
+	public void onFirstJoin(PlayerJoinEvent e) {
+		
+	}
 }
