698ff62fbb35e8a3ad8439ec94e78991c4ad5cc7
randy <randy@192.168.2.32>
2019-04-01 18:53:21 +0200

    jahh

--- COC/src/me/jerajo/coc/Main.java
+++ COC/src/me/jerajo/coc/Main.java
@@ -1,9 +1,11 @@
 package me.jerajo.coc;
 
+import org.bukkit.Bukkit;
+
 public class Main {
 
 	public void onEnabled() {
-		
+		Bukkit.getServer().getConsoleSender().sendMessage("Plugin has been enabled!");
 	}
 	
 	public void onDisabled() {
