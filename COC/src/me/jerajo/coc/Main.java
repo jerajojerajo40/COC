0be2b25666e4fea7d65a8c59d8b6dff58da7d955
randy <randy@192.168.2.32>
2019-04-01 18:58:19 +0200

    joppie

--- COC/src/me/jerajo/coc/Main.java
+++ COC/src/me/jerajo/coc/Main.java
@@ -1,14 +1,17 @@
 package me.jerajo.coc;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 
 public class Main {
+	
+	public String prefix = ChatColor.GOLD + "[COC] ";
 
 	public void onEnabled() {
-		Bukkit.getServer().getConsoleSender().sendMessage("Plugin has been enabled!");
+		Bukkit.getServer().getConsoleSender().sendMessage(prefix + ChatColor.GREEN + " Plugin has been enabled!");
 	}
 	
 	public void onDisabled() {
-		
+		Bukkit.getServer().getConsoleSender().sendMessage(prefix + ChatColor.RED + " Plugin has been disabled!");
 	}
 }
